3cd34059bea51d630b01eae2e963e4b8cf273e25
alvisjiang <alvisjiang@gmail.com>
2019-09-23 01:20:01 +0800

    Rewrite unit test with pytest

--- tests/test_application_handlers.py
+++ tests/test_application_handlers.py
@@ -1,34 +1,12 @@
 import sys
 from sanauth.core import sanauth
-from sanauth import util
-from functools import wraps
-from sanic import Sanic
 from uuid import uuid4
-from sanic.log import logger
+import pytest
 
 sys.path.insert(0, '')
 
 
-def _arg_injector(arg_type, arg_name, default):
-
-    def decorator(func):
-
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            arg_found = any(isinstance(arg, arg_type) for arg in args) \
-                        or (arg_name in kwargs and isinstance(kwargs[arg_name], arg_type))
-
-            if not arg_found:
-                kwargs[arg_name] = default() if callable(default) else default
-
-            return func(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
-
-
-def _make_test_app():
+def _test_app():
     pg_settings = dict(
         user='app-user',
         password='password',
@@ -48,83 +26,63 @@ def _make_test_app():
     return sanauth_app
 
 
-@_arg_injector(Sanic, 'sanauth_app', _make_test_app)
-def test_create_application(app_name, sanauth_app=None):
-    logger.info('testing application creation for app named: %s', app_name)
+def _client_and_app():
+    test_client = _test_app().test_client
     params = {
-        'app_name': app_name
+        'app_name': 'created_app'
     }
-    req, resp = sanauth_app.test_client.post('/app', data=params)
-
-    assert resp.status == 201
-    assert 'location' in resp.headers
-    resp_data = resp.json
-    assert resp_data
-    assert resp_data['app_name'] == app_name
-    assert resp_data['client_id']
-    assert resp_data['client_secret']
-    assert resp_data['client_id'] in resp.headers['location']
-    logger.info('all asserts passed for application creation for app named: %s', app_name)
-    return resp_data
+    req, resp = test_client.post('/app', data=params)
+    return test_client, resp.json
 
 
-def test_get_application(app_name, client_id):
+@pytest.fixture
+def client():
+    return _test_app().test_client
 
-    @_arg_injector(Sanic, 'sanauth_app', _make_test_app)
-    def test_get_application_400(sanauth_app=None):
-        logger.info('testing application query for made up app')
-        req, resp = sanauth_app.test_client.get('/app/non-existing-app-client')
+
+@pytest.fixture
+def client_id():
+    client, created_app = _client_and_app()
+    return created_app['client_id']
+
+
+class TestApplicationHandlers(object):
+
+    def test_create_application(self, client, app_name='test_app'):
+        params = {
+            'app_name': app_name
+        }
+        req, resp = client.post('/app', data=params)
+
+        assert resp.status == 201
+        assert 'location' in resp.headers
+        assert resp.json
+        assert resp.json['app_name'] == app_name
+        assert resp.json['client_id']
+        assert resp.json['client_secret']
+        assert resp.json['client_id'] in resp.headers['location']
+
+    def test_get_application_400(self, client):
+        req, resp = client.get('/app/non-existing-app-client')
         assert resp.status == 400
 
-    @_arg_injector(Sanic, 'sanauth_app', _make_test_app)
-    def test_get_application_404(sanauth_app=None):
-        req, resp = sanauth_app.test_client.get('/app/%s' % uuid4())
+    def test_get_application_404(self, client):
+        req, resp = client.get('/app/%s' % uuid4())
         assert resp.status == 404
 
-    @_arg_injector(Sanic, 'sanauth_app', _make_test_app)
-    def test_get_application_200(sanauth_app=None):
-        logger.info('testing application query for app: %s', app_name)
-        req, resp = sanauth_app.test_client.get(
-            '/app/%s' % client_id
-        )
+    def test_get_application_200(self, client, client_id):
+        req, resp = client.get('/app/%s' % client_id)
         assert resp.status == 200
-        logger.info('all asserts passed for application query for app named: %s', app_name)
 
-    test_get_application_400()
-    test_get_application_404()
-    test_get_application_200()
-
-
-def test_delete_application(client_id):
-
-    @_arg_injector(Sanic, 'sanauth_app', _make_test_app)
-    def test_delete_application_200(sanauth_app=None):
-        logger.info('testing application deletion for app: %s', client_id)
-        req, resp = sanauth_app.test_client.delete('/app/%s' % client_id)
+    def test_delete_application_200(self, client, client_id):
+        req, resp = client.delete('/app/%s' % client_id)
         assert resp.status == 200
         assert resp.json['client_id'] == client_id
-        logger.info('all asserts passed for application deletion for app: %s', client_id)
 
-    @_arg_injector(Sanic, 'sanauth_app', _make_test_app)
-    def test_delete_application_400(sanauth_app=None):
-        req, resp = sanauth_app.test_client.delete('/app/non-existing-app-client')
+    def test_delete_application_400(self, client):
+        req, resp = client.delete('/app/non-existing-app-client')
         assert resp.status == 400
 
-    @_arg_injector(Sanic, 'sanauth_app', _make_test_app)
-    def test_delete_application_404(sanauth_app=None):
-        req, resp = sanauth_app.test_client.delete('/app/%s' % uuid4())
+    def test_delete_application_404(self, client):
+        req, resp = client.delete('/app/%s' % uuid4())
         assert resp.status == 404
-
-    test_delete_application_200()
-    test_delete_application_400()
-    test_delete_application_404()
-
-
-def test_all(app_name=util.nonce_gen(32)):
-    app_data = test_create_application(app_name)
-    test_get_application(app_name, app_data['client_id'])
-    test_delete_application(app_data['client_id'])
-
-
-if __name__ == '__main__':
-    test_all()
